b6912813893ee13a1980825a865d4d0587e8daca
pracdev <davidprod210@gmail.com>
2016-09-30 23:04:19 +0200

    Devise initializer issue

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,8 +8,8 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up).permit(:name)
+    devise_parameter_sanitizer.for(:account_update).permit(:name)
   end
 
 end
